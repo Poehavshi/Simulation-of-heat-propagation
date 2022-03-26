1f00397cf3bbe9e7d87d65112ad7ac2c6dd4b778
Sotnikov Arkadiy <cotnikoarkady@gmail.com>
2022-03-26 20:36:27 +0400

    Генерация данных функции w(r, t) заданной точности
    Был создан класс для подсчёта значений функции w(r, t), который разбивает подсчёт значения функции на несколько шагов:
    - Расчёт одного единственного слагаемого
    - Расчёт суммы ряда Фурье
    - Создание кортежа из двух массивов

--- src/models/model.py
+++ src/models/model.py
@@ -8,6 +8,7 @@
 
 import numpy as np
 from scipy.special import jv, jn_zeros
+from numpy import exp
 
 
 def generate_exp_data(base, exponent):
@@ -46,18 +47,41 @@ class SumModel:
 
         :return значение одного слагаемого суммы
         """
+        mu_n = jn_zeros(0, n)[n - 1]
+        result = (5 * self.R * jv(1, self.R / 4)) / (mu_n ** 2 * (jv(1, mu_n)) ** 2)
+        result *= exp(-(t * (self.l * self.k * (mu_n / self.R) ** 2) + 2 * self.alpha) / (self.l * self.c))
+        result *= jv(0, (mu_n * r) / self.R)
+        return result
 
-    def calculate_sum(self, r: float, t: float, epsilon: float) -> float:
+    def calculate_sum(self, r: float, t: float, N: int) -> float:
         """
         Функция подсчёта значения функции w(r, t)
 
         :param r: аргумент функции w.
         :param t: аргумент функции w.
-        :param epsilon: точность, задаваеая пользователем
+        :param N: количество элементов ряда
 
         :return значение функции w(r,t)
         """
-        pass
+        result = 0
+        for i in range(N):
+            result += self._calculate_term(i + 1, r, t)
+        return result
+
+    def generate_w_data(self, t: float, N: int):
+        """
+        Генерирует значения функции w(r, t)
+
+        :param N: количество элементов (точность подсчёта функции)
+        :param t: фиксированный параметр t
+
+        :return вектор двух numpy массивов
+        """
+        r = np.linspace(0, 10, 800)
+        w = np.zeros(800)
+        for i in range(800):
+            w[i] = self.calculate_sum(r[i], t, N)
+        return r, w
 
 
 if __name__ == "__main__":
